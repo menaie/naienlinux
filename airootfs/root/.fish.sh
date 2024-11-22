9436418d103e9b9d91d007ec4c416fb287eb36c9
Menai <edumenai98@gmail.com>
2024-11-22 11:45:49 -0300

    modified:   airootfs/etc/passwd 	new file:   airootfs/etc/sddm.conf 	new file:   airootfs/etc/sddm.conf.d/kde_settings.conf 	new file:   airootfs/etc/sddm.conf.d/wayland.conf 	new file:   airootfs/root/.fish.sh 	new file:   airootfs/root/.naienlinux.sh 	modified:   airootfs/root/.zlogin

---
+++ airootfs/root/.fish.sh
@@ -0,0 +1,3 @@
+#!/bin/fish
+
+set -U fish_greeting
---
+++ airootfs/root/.naienlinux.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo systemctl enable NetworkManager.service
+sudo systemctl enable bluetooth.service
+sudo systemctl enable sddm.service
+
+
