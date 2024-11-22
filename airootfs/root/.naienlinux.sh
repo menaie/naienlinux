c4729ee38075a066243434d5c0aa9fca5efa8d14
Menai <edumenai98@gmail.com>
2024-11-22 19:05:13 -0300

    modified:   airootfs/root/.naienlinux.sh 	modified:   packages.x86_64

--- airootfs/root/.naienlinux.sh
+++ airootfs/root/.naienlinux.sh
@@ -3,5 +3,8 @@
 sudo systemctl enable NetworkManager.service
 sudo systemctl enable bluetooth.service
 
+sudo pacman -U --noconfirm /packages/calamares-3.3.8-1-x86_64.pkg.tar.zst
+sudo pacman -U --noconfirm /packages/mkinitcpio-openswap-0.1.0-3-any.pkg.tar.zst
+
 
 
