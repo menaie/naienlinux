e6fe8d97ec7f4d00a6bc4f027a7a4a62f3ddb968
Menai <edumenai98@gmail.com>
2024-11-25 20:49:08 -0300

    modified:   airootfs/root/.calkde.sh 	renamed:    airootfs/usr/share/wallpapers/naienlinux-wallpaper.png -> airootfs/usr/share/wallpapers/naienlinux/naienlinux-wallpaper.png 	modified:   packages.x86_64

--- airootfs/root/.calkde.sh
+++ airootfs/root/.calkde.sh
@@ -1,4 +1,10 @@
-#!/bin/zsh
+#!/bin/bash
 
 rm /usr/share/applications/calamares.desktop
 mv /usr/share/applications/calamares1.desktop /usr/share/applications/calamares.desktop
+
+WALLPAPER_PATH="/usr/share/wallpapers/naienlinux-wallpaper.png"
+PLASMA_CONFIG_DIR="/home/naien/.config"
+mkdir -p $PLASMA_CONFIG_DIR cat > $PLASMA_CONFIG_DIR/plasma-org.kde.plasma.desktop-appletsrc <<EOL [Containments][1][Wallpaper][org.kde.image][General] Image=file://$WALLPAPER_PATH EOL 
+echo "Wallpaper set to $WALLPAPER_PATH"
+
