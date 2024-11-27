8a1cc98af96e7b50f026ade2b642de596eae9d5b
Menai <edumenai98@gmail.com>
2024-11-26 21:44:21 -0300

    modified:   airootfs/etc/skel/.config/kdedefaults/kdeglobals 	renamed:    airootfs/etc/skel/.local/share/color-schemes/naienlinux.colors -> airootfs/etc/skel/.local/share/color-schemes/Naienlinux.colors 	modified:   airootfs/etc/skel/Desktop/Install_System.desktop 	modified:   airootfs/root/.calkde.sh

--- airootfs/root/.calkde.sh
+++ airootfs/root/.calkde.sh
@@ -2,10 +2,5 @@
 
 rm /usr/share/applications/calamares.desktop
 mv /usr/share/applications/calamares1.desktop /usr/share/applications/calamares.desktop
-chmod +x /home/naien/Desktop/Install_System.desktop
-
-WALLPAPER_PATH="/usr/share/wallpapers/naienlinux-wallpaper.png"
-PLASMA_CONFIG_DIR="/home/naien/.config"
-mkdir -p $PLASMA_CONFIG_DIR cat > $PLASMA_CONFIG_DIR/plasma-org.kde.plasma.desktop-appletsrc <<EOL [Containments][1][Wallpaper][org.kde.image][General] Image=file://$WALLPAPER_PATH EOL 
-echo "Wallpaper set to $WALLPAPER_PATH"
+chmod +x /home/*/Desktop/Install_System.desktop
 
